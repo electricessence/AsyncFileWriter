c28eb593c7eb463dfafdaa4c2484c3720cc108f0
Oren (electricessence) <oren@electrified.net>
2018-04-06 01:09:15 -0700

    V1

--- AsyncFileWriter/AsyncFileWriter.cs
+++ AsyncFileWriter/AsyncFileWriter.cs
@@ -1,65 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
-namespace AsyncFileWriter
+namespace Open
 {
-    public class AsyncFileWriter
-    {
-        public readonly string FilePath;
-        readonly BufferBlock<string> _buffer;
-        ActionBlock<string> _writer;
+	public class AsyncFileWriter : ITargetBlock<byte[]>, IDisposable
+	{
+		public readonly string FilePath;
+		readonly BufferBlock<byte[]> _buffer;
+		Task<Task> _writerCompletion;
 
-        public AsyncFileWriter(string filePath)
-        {
-            FilePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
-            _buffer = new BufferBlock<string>();
-        }
+		public AsyncFileWriter(string filePath)
+		{
+			FilePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
+			_buffer = new BufferBlock<byte[]>();
+			Completion = CreateCompletion();
+		}
 
-        public void Write(string value)
-        {
-            _buffer.Post(value);
-            EnsureWriter();
-        }
+		public void Dispose()
+		{
+			Complete();
+			Completion.ContinueWith(t => { /* Ignore fault */ }).Wait();
+		}
 
-        void EnsureWriter()
-        {
-            if (_buffer.Count != 0)
-            {
-                LazyInitializer.EnsureInitialized(ref _writer, () =>
-                {
-                    var fs = File.AppendText(FilePath);
-                    var writer = new ActionBlock<string>(fs.WriteAsync);
-                    writer.Completion.ContinueWith(t =>
-                    {
-                        fs.Dispose();
-                        Interlocked.Exchange(ref _writer, null);
-                        EnsureWriter();
-                    });
+		// Allow propagation of faults.
+		Task CreateCompletion()
+			=> _buffer
+			.Completion
+			.ContinueWith(
+				t1 => _writerCompletion
+					?.Unwrap()
+					?.ContinueWith(t2 => _buffer.Completion).Unwrap()
+					?? _buffer.Completion)
+			.Unwrap();
 
-                    Task.Run(() =>
-                    {
-                        while(_buffer.TryReceiveAll(out IList<string> entries))
-                        {
-                            foreach (var e in entries)
-                                writer.Post(e);
-                        }
+		public void Complete() => _buffer.Complete();
 
-                        // No more for now... 
-                        writer.Complete();
-                    });
+		public Task Completion { get; private set; }
 
-                    return writer;
-                });
-            }
-        }
+		Task InitWriter()
+		{
+			FileStream fs = null;
+			try
+			{
+				Debug.WriteLine($"Initializing FileStream: {FilePath}");
+				fs = new FileStream(FilePath, FileMode.Append, FileAccess.Write, FileShare.None, 4096, true);
 
-        public void WriteLine(string value)
-        {
-            Write((value ?? string.Empty) + '\n');
-        }
-    }
+				IDisposable link = null;
+				ActionBlock<byte[]> writer = null;
+				writer = new ActionBlock<byte[]>(async bytes =>
+				{
+					await fs.WriteAsync(bytes, 0, bytes.Length);
+					if (_buffer.Count == 0 && writer.InputCount == 0)
+					{
+						link.Dispose(); // Unlink immediately...
+						writer.Complete();
+					}
+				});
+
+				link = _buffer.LinkTo(writer, new DataflowLinkOptions { PropagateCompletion = true }); // Begin consumption immediately...
+
+				return writer.Completion.ContinueWith(t =>
+				{
+					link.Dispose();
+					if (t.IsFaulted) Fault(t.Exception);
+					Debug.WriteLine($"Disposing FileStream. {FilePath}");
+					fs.Dispose();
+					Interlocked.Exchange(ref _writerCompletion, null);
+					EnsureWriter();
+				});
+			}
+			catch
+			{
+				Debug.WriteLine($"Initializing FileStream FAILED");
+				fs?.Dispose();
+				Interlocked.Exchange(ref _writerCompletion, null); // Allow for retrying...
+				throw;
+			}
+		}
+
+		void EnsureWriter()
+		{
+			if (_buffer.Count != 0)
+			{
+				Task<Task> t = null;
+				var task = LazyInitializer.EnsureInitialized(ref _writerCompletion,
+					() => t = new Task<Task>(InitWriter));
+				if (task == t) task.Start();  // Are we the thread/task that is the creator?
+			}
+		}
+
+		public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, byte[] messageValue, ISourceBlock<byte[]> source, bool consumeToAccept)
+		{
+			var status = ((ITargetBlock<byte[]>)_buffer).OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+			if (status == DataflowMessageStatus.Accepted) EnsureWriter();
+			return status;
+		}
+
+		public void Fault(Exception exception)
+		{
+			((ITargetBlock<byte[]>)_buffer).Fault(exception);
+		}
+	}
 }
---
+++ AsyncFileWriterTester/Program.cs
@@ -0,0 +1,64 @@
+﻿using Open;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace AsyncFileWriterTester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var dir = Environment.CurrentDirectory;
+			var fileName = args.FirstOrDefault() ?? "AsyncFileWriterTest.txt";
+			var filePath = Path.Combine(dir, fileName);
+			File.Delete(filePath); // Start from scratch. (comment out for further appends.)
+
+			Console.WriteLine($"Writing to file: {filePath}");
+			using (var writer = new AsyncFileWriter(filePath))
+			{
+
+				// Test to ensure blocks are linkable and properly pass messages.
+				var buffer = new TransformBlock<string, byte[]>(
+					value=>Encoding.UTF8.GetBytes(value +'\n'),
+					new ExecutionDataflowBlockOptions {
+						BoundedCapacity = 200000 // Test a max pre-buffered amount here.
+					});
+
+				buffer.LinkTo(writer, new DataflowLinkOptions { PropagateCompletion = true });
+				writer.Completion.ContinueWith(t => {
+					buffer.Complete();
+					buffer.LinkTo(DataflowBlock.NullTarget<byte[]>()); // Empty the buffer and allow the buffer to complete.
+				});
+
+				void write(int i)
+				{
+					var message = $"{i}) {DateTime.Now}";
+					if(!buffer.Post(message) && !buffer.Completion.IsCompleted)
+						buffer.SendAsync(message).Wait();
+				}
+
+				Parallel.For(0, 10000, write);
+				Parallel.For(10000, 20000, write);
+		
+				//writer.Fault(new Exception("Stop!"));
+
+				Task.Delay(1).Wait();
+				Parallel.For(20000, 100000, write);
+
+				Task.Delay(1000).Wait(); // Demonstrate that when nothing buffered the active stream closes.
+				Parallel.For(100000, 1000000, write);
+
+				buffer.Complete();
+				buffer.Completion.Wait();
+
+				if (writer.Completion.IsFaulted)
+					throw writer.Completion.Exception;
+			}
+		}
+	}
+}
