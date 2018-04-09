a09ca213f8cf9f1fe63d1a542d24480e8d847aad
electricessence <oren@electrified.net>
2018-04-08 19:39:03 -0700

    Updates.

--- AsyncFileWriter/AsyncFileWriter.cs
+++ AsyncFileWriter/AsyncFileWriter.cs
@@ -8,19 +8,21 @@ using System.Threading.Tasks.Dataflow;
 
 namespace Open
 {
-	public class AsyncFileWriter : ITargetBlock<byte[]>, ITargetBlock<char[]>, ITargetBlock<string>, IDisposable
+	public class AsyncFileWriter
+		: ITargetBlock<byte[]>, ITargetBlock<char[]>, ITargetBlock<string>, IDisposable
 	{
 		public readonly string FilePath;
 		public readonly Encoding Encoding;
-		public readonly int BoundedCapacity;
 		readonly ActionBlock<byte[]> _writer;
 		Lazy<FileStream> _fileStream;
 		bool _completeCalled; // Need to allow for postponed messages to be processed.
 
-		public AsyncFileWriter(string filePath, int boundedCapacity = -1, Encoding encoding = null)
+		#region Constructors
+		AsyncFileWriter(string filePath,
+			Encoding encoding,
+			ExecutionDataflowBlockOptions options = null)
 		{
 			FilePath = filePath ?? throw new ArgumentNullException(nameof(filePath));
-			BoundedCapacity = boundedCapacity;
 			Encoding = encoding ?? Encoding.UTF8;
 
 			_writer = new ActionBlock<byte[]>(async bytes =>
@@ -28,32 +30,31 @@ namespace Open
 				await GetFileStream().Value.WriteAsync(bytes, 0, bytes.Length);
 				if (_writer.InputCount == 0) OnWriterEmpty();
 			},
-			new ExecutionDataflowBlockOptions
-			{
-				BoundedCapacity = boundedCapacity
-			});
+			options);
 
 			_writer.Completion
 				.ContinueWith(OnWriterEmpty);
 		}
 
-		public AsyncFileWriter(string filePath, Encoding encoding, int boundedCapacity = -1) : this(filePath, boundedCapacity, encoding)
+		public AsyncFileWriter(string filePath, Encoding encoding = null, TaskScheduler taskScheduler = null, int boundedCapacity = DataflowBlockOptions.Unbounded)
+		: this(filePath, encoding, new ExecutionDataflowBlockOptions
 		{
+			TaskScheduler = taskScheduler,
+			BoundedCapacity = boundedCapacity
+		})
+		{ }
 
-		}
+		public AsyncFileWriter(string filePath, TaskScheduler taskScheduler, int boundedCapacity = DataflowBlockOptions.Unbounded)
+		: this(filePath, null, taskScheduler, boundedCapacity) { }
 
-		/// <summary>
-		/// Signals completion and waits for all bytes to be written to the destination.
-		/// If immediately cancellation of activity is required, call .CompleteImmediate() before disposing.
-		/// </summary>
-		public void Dispose()
-		{
-			Complete();
-			_writer.Completion
-				.ContinueWith(t => { /* Ignore fault */ })
-				.Wait();
-		}
+		public AsyncFileWriter(string filePath, Encoding encoding, int boundedCapacity)
+		: this(filePath, encoding, null, boundedCapacity) { }
 
+		public AsyncFileWriter(string filePath, int boundedCapacity)
+		: this(filePath, null, null, boundedCapacity) { }
+		#endregion
+
+		#region Completion
 		/// <summary>
 		/// The task that indicates completion.
 		/// </summary>
@@ -62,7 +63,13 @@ namespace Open
 		/// <summary>
 		/// Signals that no more bytes should be accepted and signal completion once all the bytes have been written.
 		/// </summary>
-		public void Complete() => _completeCalled = true;
+		public void Complete()
+		{
+			_completeCalled = true;
+			// Avoid tossing out postponed...
+			if (_writer.InputCount == 0)
+				_writer.Complete();
+		}
 
 		/// <summary>
 		/// Stops all processing of bytes and signals completion.
@@ -72,6 +79,7 @@ namespace Open
 			Complete();
 			_writer.Complete();
 		}
+		#endregion
 
 		Lazy<FileStream> GetFileStream()
 		{
@@ -95,10 +103,11 @@ namespace Open
 			}
 		}
 
+		#region OfferMessage
 		public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, byte[] messageValue, ISourceBlock<byte[]> source, bool consumeToAccept)
 			=> _completeCalled
-				? DataflowMessageStatus.DecliningPermanently
-				: ((ITargetBlock<byte[]>)_writer).OfferMessage(messageHeader, messageValue, source, consumeToAccept);
+			? DataflowMessageStatus.DecliningPermanently
+			: ((ITargetBlock<byte[]>)_writer).OfferMessage(messageHeader, messageValue, source, consumeToAccept);
 
 		// Might be able to build a proxy to translate the source block to the proper values and allow consumption.
 		public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, char[] messageValue, ISourceBlock<char[]> source, bool consumeToAccept)
@@ -107,6 +116,8 @@ namespace Open
 		public DataflowMessageStatus OfferMessage(DataflowMessageHeader messageHeader, string messageValue, ISourceBlock<string> source, bool consumeToAccept)
 			=> OfferMessage(messageHeader, Encoding.GetBytes(messageValue), null, false);
 
+		#endregion
+
 		public void Fault(Exception exception)
 		{
 			((ITargetBlock<byte[]>)_writer).Fault(exception);
@@ -114,5 +125,45 @@ namespace Open
 
 		public bool PostLine(string line = null)
 			=> this.Post((line ?? string.Empty) + '\n');
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			Complete();
+			if (!disposedValue)
+			{
+				if (!disposing) // Garbage collecting?
+				{
+					_writer.Complete();
+				}
+
+				_writer.Completion
+					.ContinueWith(t => { /* Ignore fault */ })
+					.Wait();
+
+				disposedValue = true;
+			}
+		}
+
+		~AsyncFileWriter()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// Signals completion and waits for all bytes to be written to the destination.
+		/// If immediately cancellation of activity is required, call .CompleteImmediate() before disposing.
+		/// </summary>
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			GC.SuppressFinalize(this);
+		}
+		#endregion
 	}
 }
